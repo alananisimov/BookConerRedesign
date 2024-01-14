c5087ae36dff6c3a7c2c636cb101389dbac8df17
alananisimov <alananisimov@gmail.com>
2024-01-14 14:41:53 +0300

    123

--- src/entities/Layout/nav.tsx
+++ src/entities/Layout/nav.tsx
@@ -1,5 +1,5 @@
 import { authOptions } from "src/app/lib/configuration";
-import Navbar from "./Navbar";
+import Navbar from "./navbar";
 import { getServerSession } from "next-auth/next";
 
 export default async function Nav() {
